790ce71930bda04755f01d4ed1bfac4cf3a964b0
freya <pxz13532595152@163.com>
2024-01-31 16:42:34 +0800

    投注面板添加动画

--- src/views/betPage.vue
+++ src/views/betPage.vue
@@ -33,63 +33,62 @@
             </div>
         </div>
         <!-- 投注面板 -->
-        <div class="betPanel" v-if="isShowBetPanel">
-            <van-floating-panel :content-draggable="false">
-                <div class="panel_top">
-                    <div class="flex left_box">
-                        <div class="money">
-                            <img :src="getImg('header', 'mIcon')" class="mIcon" alt="">
-                            <span class="moneyNum">5093.76</span>
-                        </div>
-                        <div class="panel_tit">Bet Slip</div>
+        <div class="betPanel maxWidth" :class="{ showBetPanel: isShowBetPanel }">
+            <div class="panelBar"></div>
+            <div class="panel_top">
+                <div class="flex left_box">
+                    <div class="money">
+                        <img :src="getImg('header', 'mIcon')" class="mIcon" alt="">
+                        <span class="moneyNum">5093.76</span>
                     </div>
-                    <van-icon name="cross" color="#fff" />
+                    <div class="panel_tit">Bet Slip</div>
                 </div>
-                <div class="panel_main">
-                    <div class="clubs_name flex">
-                        <img src="../assets/images/contest/fIcon.webp" alt="">
-                        <p>Friendlies Clubs</p>
-                    </div>
-                    <div class="team flex">
-                        <p class="flex">
-                            <img src="../assets/images/betpage/mainIcon.webp" alt="">
-                            Liverpool
-                        </p>
-                        <span>vs</span>
-                        <p class="flex">
-                            <img src="../assets/images/betpage/guest.webp" alt="">
-                            Aston Villa
-                        </p>
-                    </div>
-                    <div class="betDetal">
-                        <p class="betScore">
-                            <span>Events</span>
-                            <span>0-1</span>
-                        </p>
-                        <p class="betOdds">
-                            <span>Odds</span>
-                            <span>1.83</span>
-                        </p>
-                    </div>
-                    <p class="betRange">Betting Range: Minimum 10.00 - 10000.00</p>
-                    <p class="betWinNum">Potential winnings: <span>18.30</span></p>
-                    <div class="counter">
-                        <div class="count-left flex">
-                            <input type="number" v-model="betNum">
-                            <img src="../assets/images/betpage/subtraction.webp" class="subtraction" alt="">
-                            <img src="../assets/images/betpage/add.webp" class="add" alt="">
-                        </div>
-                        <div class="betBtn">Bet</div>
-                    </div>
-                    <p class="serviceFee">Service Fee：<span class="num">0</span></p>
-                    <p class="quickBet">Quick bets</p>
-                    <div class="betButton">
-                        <div>100</div>
-                        <div>200</div>
-                        <div>ALL</div>
-                    </div>
+                <van-icon name="cross" color="#fff" @click="closePanel" />
+            </div>
+            <div class="panel_main">
+                <div class="clubs_name flex">
+                    <img src="../assets/images/contest/fIcon.webp" alt="">
+                    <p>Friendlies Clubs</p>
                 </div>
-            </van-floating-panel>
+                <div class="team flex">
+                    <p class="flex">
+                        <img src="../assets/images/betpage/mainIcon.webp" alt="">
+                        Liverpool
+                    </p>
+                    <span>vs</span>
+                    <p class="flex">
+                        <img src="../assets/images/betpage/guest.webp" alt="">
+                        Aston Villa
+                    </p>
+                </div>
+                <div class="betDetal">
+                    <p class="betScore">
+                        <span>Events</span>
+                        <span>0-1</span>
+                    </p>
+                    <p class="betOdds">
+                        <span>Odds</span>
+                        <span>1.83</span>
+                    </p>
+                </div>
+                <p class="betRange">Betting Range: Minimum 10.00 - 10000.00</p>
+                <p class="betWinNum">Potential winnings: <span>18.30</span></p>
+                <div class="counter">
+                    <div class="count-left flex">
+                        <input type="number" v-model="betNum">
+                        <img src="../assets/images/betpage/subtraction.webp" class="subtraction" alt="">
+                        <img src="../assets/images/betpage/add.webp" class="add" alt="">
+                    </div>
+                    <div class="betBtn">Bet</div>
+                </div>
+                <p class="serviceFee">Service Fee：<span class="num">0</span></p>
+                <p class="quickBet">Quick bets</p>
+                <div class="betButton">
+                    <div>100</div>
+                    <div>200</div>
+                    <div>ALL</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -183,6 +182,9 @@ const state = reactive({
     betNum: 10,
     isShowBetPanel: false
 })
+function closePanel() {
+    state.isShowBetPanel = false
+}
 function handleBetClick(item, index) {
     console.log(index);
     state.betIndex = index
@@ -375,234 +377,236 @@ $bgHeight: 280px;
     }
 
     .betPanel {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        height: 0;
         background-color: #1c1b20;
+        box-sizing: border-box;
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
+        transition: height .5s ease-out;
 
-        :deep(.van-floating-panel) {
-            background-color: #1c1b20;
+        .panelBar {
+            width: 35px;
+            height: 5px;
+            margin: 8px auto 10px;
+            border-radius: 3px;
+            background-color: #3b3a42;
+        }
 
-            .van-floating-panel__header {
-                .van-floating-panel__header-bar {
-                    width: 35px;
-                    height: 5px;
-                    border-radius: 3px;
-                    background-color: #3b3a42;
+        .panel_top {
+            padding: 0 16px;
+            @include flex();
+            border-bottom: 1px solid #313038;
+            padding-bottom: 15px;
+
+            .left_box {
+                align-items: center;
+
+                .money {
+                    width: fit-content;
+                    height: 32px;
+                    @include flex();
+                    margin-right: 23px;
+                    border-radius: 16px;
+                    background-color: #2b2b2b;
+                    padding: 0px 9px 0px 4px;
+
+                    .moneyNum {
+                        margin: 0px 0 0px 6px;
+                        font-family: Roboto;
+                        font-size: 14px;
+                        color: #fff;
+                    }
+
+                    .mIcon {
+                        width: 24px;
+                        height: 24px;
+                    }
+                }
+
+                .panel_tit {
+                    font-family: SegoeUI;
+                    font-size: 17px;
+                    font-weight: 600;
+                    letter-spacing: -0.17px;
+                    color: #fff;
+                    margin-left: 15px;
+                }
+
+            }
+        }
+
+        .panel_main {
+            padding: 0 16px;
+
+            .clubs_name {
+                margin-top: 18px;
+                align-items: center;
+                padding-left: 5px;
+
+                img {
+                    width: 16px;
+                    height: 16px;
+                }
+
+                p {
+                    font-size: 14px;
+                    color: #8d8d8d;
+                    margin-left: 5px;
+                }
+            }
+
+            .team {
+                align-items: center;
+                margin-top: 15px;
+
+                p {
+                    align-items: center;
+                    font-size: 14px;
+                    color: #fff;
+
+                    img {
+                        width: 22px;
+                        height: 22px;
+                    }
+                }
+
+                span {
+                    font-size: 12px;
+                    color: #fff;
+                    margin: 0 11px 0;
+                }
+            }
+
+            .betDetal {
+                border-radius: 8px;
+                background-color: #24232a;
+                padding: 9px 10px 8px 12px;
+                margin-top: 15px;
+
+                p {
+                    @include flex(space-between);
+                    font-size: 14px;
+                    color: #9f9f9f;
+
+                    span:last-child {
+                        font-size: 14px;
+                        font-weight: bold;
+                        letter-spacing: -0.14px;
+                        color: #fff;
+                    }
+                }
+
+                .betOdds {
+                    margin-top: 5px;
 
                 }
             }
 
-            .van-floating-panel__content {
-                background-color: #1c1b20;
+            .betRange {
+                font-size: 14px;
+                color: #9f9f9f;
+                margin-top: 16px;
+            }
 
-                .panel_top {
-                    padding: 0 16px;
-                    @include flex();
-                    border-bottom: 1px solid #313038;
-                    padding-bottom: 15px;
+            .betWinNum {
+                margin: 7px 0 12px 0;
+                font-family: SegoeUI;
+                font-size: 14px;
+                letter-spacing: -0.14px;
+                color: #fff;
 
-                    .left_box {
-                        align-items: center;
+                span {
+                    font-weight: bold;
+                }
+            }
 
-                        .money {
-                            width: fit-content;
-                            height: 32px;
-                            @include flex();
-                            margin-right: 23px;
-                            border-radius: 16px;
-                            background-color: #2b2b2b;
-                            padding: 0px 9px 0px 4px;
+            .counter {
+                @include flex(space-between);
 
-                            .moneyNum {
-                                margin: 0px 0 0px 6px;
-                                font-family: Roboto;
-                                font-size: 14px;
-                                color: #fff;
-                            }
+                .count-left {
+                    flex: 1;
+                    height: 36px;
+                    background-color: #24232a;
+                    align-items: center;
+                    padding-right: 5px;
+                    border-radius: 8px;
 
-                            .mIcon {
-                                width: 24px;
-                                height: 24px;
-                            }
-                        }
+                    input {
+                        width: 150px;
+                        border: none;
+                        outline: none;
+                        background-color: #24232a;
+                        color: #fff;
+                        padding-left: 12px;
+                    }
 
-                        .panel_tit {
-                            font-family: SegoeUI;
-                            font-size: 17px;
-                            font-weight: 600;
-                            letter-spacing: -0.17px;
-                            color: #fff;
-                            margin-left: 15px;
-                        }
+                    img {
+                        width: 28px;
+                        height: 28px;
+                    }
 
+                    .subtraction {}
+
+                    .add {
+                        margin-left: 8px;
                     }
                 }
 
-                .panel_main {
-                    padding: 0 16px;
+                .betBtn {
+                    width: 110px;
+                    height: 36px;
+                    text-align: center;
+                    line-height: 36px;
+                    margin: 0 4px 0 7px;
+                    border-radius: 8px;
+                    background-color: #ff7c43;
+                    font-size: 14px;
+                    font-weight: bold;
+                    letter-spacing: -0.14px;
+                    color: #fff;
+                }
+            }
 
-                    .clubs_name {
-                        margin-top: 18px;
-                        align-items: center;
-                        padding-left: 5px;
+            .serviceFee {
+                margin-top: 8px;
+                font-size: 14px;
+                color: #a6a9b8;
 
-                        img {
-                            width: 16px;
-                            height: 16px;
-                        }
+                span {
+                    color: #fff;
+                }
+            }
 
-                        p {
-                            font-size: 14px;
-                            color: #8d8d8d;
-                            margin-left: 5px;
-                        }
-                    }
+            .quickBet {
+                font-size: 14px;
+                color: #e2e2e2;
+                margin-top: 12px;
+            }
 
-                    .team {
-                        align-items: center;
-                        margin-top: 15px;
+            .betButton {
+                @include flex();
+                margin-top: 6px;
 
-                        p {
-                            align-items: center;
-                            font-size: 14px;
-                            color: #fff;
-
-                            img {
-                                width: 22px;
-                                height: 22px;
-                            }
-                        }
-
-                        span {
-                            font-size: 12px;
-                            color: #fff;
-                            margin: 0 11px 0;
-                        }
-                    }
-
-                    .betDetal {
-                        border-radius: 8px;
-                        background-color: #24232a;
-                        padding: 9px 10px 8px 12px;
-                        margin-top: 15px;
-
-                        p {
-                            @include flex(space-between);
-                            font-size: 14px;
-                            color: #9f9f9f;
-
-                            span:last-child {
-                                font-size: 14px;
-                                font-weight: bold;
-                                letter-spacing: -0.14px;
-                                color: #fff;
-                            }
-                        }
-
-                        .betOdds {
-                            margin-top: 5px;
-
-                        }
-                    }
-
-                    .betRange {
-                        font-size: 14px;
-                        color: #9f9f9f;
-                        margin-top: 16px;
-                    }
-
-                    .betWinNum {
-                        margin: 7px 0 12px 0;
-                        font-family: SegoeUI;
-                        font-size: 14px;
-                        letter-spacing: -0.14px;
-                        color: #fff;
-
-                        span {
-                            font-weight: bold;
-                        }
-                    }
-
-                    .counter {
-                        @include flex(space-between);
-
-                        .count-left {
-                            flex: 1;
-                            height: 36px;
-                            background-color: #24232a;
-                            align-items: center;
-                            padding-right: 5px;
-                            border-radius: 8px;
-
-                            input {
-                                width: 150px;
-                                border: none;
-                                outline: none;
-                                background-color: #24232a;
-                                color: #fff;
-                                padding-left: 12px;
-                            }
-
-                            img {
-                                width: 28px;
-                                height: 28px;
-                            }
-
-                            .subtraction {}
-
-                            .add {
-                                margin-left: 8px;
-                            }
-                        }
-
-                        .betBtn {
-                            width: 110px;
-                            height: 36px;
-                            text-align: center;
-                            line-height: 36px;
-                            margin: 0 4px 0 7px;
-                            border-radius: 8px;
-                            background-color: #ff7c43;
-                            font-size: 14px;
-                            font-weight: bold;
-                            letter-spacing: -0.14px;
-                            color: #fff;
-                        }
-                    }
-
-                    .serviceFee {
-                        margin-top: 8px;
-                        font-size: 14px;
-                        color: #a6a9b8;
-
-                        span {
-                            color: #fff;
-                        }
-                    }
-
-                    .quickBet {
-                        font-size: 14px;
-                        color: #e2e2e2;
-                        margin-top: 12px;
-                    }
-
-                    .betButton {
-                        @include flex();
-                        margin-top: 6px;
-
-                        div {
-                            width: 110px;
-                            height: 36px;
-                            text-align: center;
-                            line-height: 36px;
-                            margin: 0 5px 14px 0px;
-                            border-radius: 8px;
-                            background-color: #4d5fff;
-                            font-size: 14px;
-                            color: #fff;
-                        }
-                    }
+                div {
+                    width: 110px;
+                    height: 36px;
+                    text-align: center;
+                    line-height: 36px;
+                    margin: 0 5px 14px 0px;
+                    border-radius: 8px;
+                    background-color: #4d5fff;
+                    font-size: 14px;
+                    color: #fff;
                 }
             }
         }
+    }
+
+    .showBetPanel {
+        height: 460px;
     }
 }
 </style>
