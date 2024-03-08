7868e57b8e4da2d0e7c8966bdbb9e17626d55d0f
lhj501382150 <liuhaojie113@163.com>
2024-03-08 17:34:19 +0800

    下载页面

--- src/views/download.vue
+++ src/views/download.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="download">
-
+        <div class="logo">
+            <img :src="getImg('down', 'icon')" />
+        </div>
+        <div class="btn-div">
+            <input type="button" :value="$t('fuc.app.download')" @click="getDownObj()" class="btn" />
+        </div>
     </div>
 </template>
 <script setup>
  import { reactive, toRefs, onMounted, watchEffect } from 'vue'
 import { GridItem, showToast } from 'vant'
+import { getImg } from '@/utils/utils'
 import { useRouter } from 'vue-router'
 import http from '@/utils/axios'
 import { useStore } from '@/stores/index'
@@ -16,13 +22,18 @@ const router = useRouter()
 
 const state = reactive({
     osType:'',
-    downObj:{}
+    downObj:{},
+    loading:false
 })
 getOsType()
-getDownObj()
+//getDownObj()
 async function getDownObj(){
+    if(state.loading){
+        return
+    }
     let url ='/player/home/app_url'
     const res = await http.get(url)
+    state.loading = true
     res.forEach(item => {
         if(item.appType==state.osType){
             state.downObj = item
@@ -44,6 +55,7 @@ function getOsType() {
         state.osType = 0
     }
 }
+const { osType, downObj, loading } = toRefs(state)
 </script>
 <style scoped lang="scss">
 .download {
@@ -52,5 +64,26 @@ function getOsType() {
     box-sizing: border-box;
     padding-top: 5px;
     overflow-y: auto;
+    background-image: url('../assets/images/down/bg.webp');
+    background-size: 100% 100%;
+    @include flex(spave-around,center);
+    flex-direction: column;
+    align-items: center;
+    .logo{
+        width: 88px;
+        height:113px;
+        margin-top: 15vh;
+    }
+    .btn-div{
+        margin-top: 35vh;
+        .btn{
+            width: 267px;
+            height: 48px;
+            border-radius: 24px;
+            box-shadow: 0 3px 6px 0 rgba(245, 195, 130, 0.51);
+            background-image: linear-gradient(to right, #fbe5c9, #f4c07d);
+            text-transform: uppercase;
+        }
+    }
 }
 </style>
