e6fddd94a761e767c54c28a73519fdd033383f2f
attoote <119163844+attoote@users.noreply.github.com>
2024-07-08 09:46:05 +0700

    fix

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.config = {
   UNIFIED_NUMBER: 10000,
   USDT_NUMBER: 10000,
-  baseUrl: "https://api82asuyr34ng7.myfar.cc",
+  baseUrl: "https://api.uunn.org",
   leftTitle:"ARC.CC"
   //   MSG_BELL: "1",
 };
