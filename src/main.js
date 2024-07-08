516ed3da7a4ded9c1770e506b1779980822d949d
attoote <119163844+attoote@users.noreply.github.com>
2024-07-08 20:36:01 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -11,9 +11,10 @@ import router from "./router";
 import i18n from "./i18n/i18n";
 
 import { Toast } from "vant";
-
+import { Locale } from 'vant';
+import enUS from 'vant/es/locale/lang/en-US'; 
 const app = createApp(App);
-
+Locale.use('en-US', enUS);
 app.use(Toast);
 
 app.use(createPinia());
